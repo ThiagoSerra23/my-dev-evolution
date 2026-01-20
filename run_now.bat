3db8e9281181e08ff410394ca9fba8ada48f0387
ThiagoSerra23 <thiagopserrasantos@gmail.com>
2026-01-20 15:41:38 -0300

    run.bat

--- run_now.bat
+++ run_now.bat
@@ -1,4 +1,5 @@
 @echo off
+chcp 65001 > nul
 echo --- Dev Daily Evolution: Execucao Manual ---
 echo.
 
@@ -9,10 +10,14 @@ echo.
 echo --- Preparando para enviar ao GitHub ---
 echo.
 
-:: Pega a mensagem do ultimo commit gerado
-set /p COMMIT_MSG=<scripts/last_commit_msg.txt
+:: Pega a mensagem do ultimo commit usando o script Python (mais seguro)
+for /f "delims=" %%i in ('python scripts/get_commit_message.py') do set COMMIT_MSG=%%i
+
+echo Mensagem do commit: %COMMIT_MSG%
+echo.
 
 :: Git commands
+git pull
 git add .
 git commit -m "%COMMIT_MSG%"
 git push
